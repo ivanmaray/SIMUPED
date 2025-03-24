7d8bfe3d6697d3dc1750aa21da4c26417c2e5b0b
Iván Maray Mateos <ivanmaray@Ordenador-portatil-de-Ivan.local>
2025-03-24 21:08:41 +0100

    Mover index.html a raíz del proyecto para Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // ← asegúrate de que el proyecto empieza desde la raíz
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
