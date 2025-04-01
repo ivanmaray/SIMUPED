6f9dd3477adf761db1e6fae7c9194d03404c9d76
Iván Maray Mateos <ivanmaray@Ordenador-portatil-de-Ivan.local>
2025-04-01 22:39:53 +0200

    Actualizar logo

--- src/components/SelectorModalidad.jsx
+++ src/components/SelectorModalidad.jsx
@@ -5,10 +5,10 @@ import AnimatedButton from './AnimatedButton';
 export default function SelectorModalidad({ onSelect }) {
   return (
     <div className="flex flex-col items-center justify-center py-5 bg-transparent">
-      <h1 className="text-4xl font-bold text-blue-900 mb-2">Selecciona la Modalidad</h1>
-      <div className="flex space-x-4">
-        <div className="flex flex-col items-center">
-          <h2 className="text-xl font-semibold mb-1">Simulación en Directo</h2>
+      <h1 className="text-4xl font-bold text-blue-900 mb-4">Selecciona la Modalidad</h1>
+      <div className="flex space-x-8">
+        <div className="flex flex-col items-center space-y-4">
+          <h2 className="text-xl font-semibold">Simulación en Directo</h2>
           <AnimatedButton
             onClick={() => onSelect("directo")}
             className="bg-green-600 text-white px-4 py-2 rounded shadow-lg hover:bg-green-700"
@@ -16,7 +16,7 @@ export default function SelectorModalidad({ onSelect }) {
             Versión en Directo
           </AnimatedButton>
           {/* Video/gif para modalidad en directo */}
-          <div className="mt-2 w-64 h-36 bg-gray-200 flex items-center justify-center">
+          <div className="w-64 h-36 bg-gray-200 flex items-center justify-center">
             <img
               src="/videos/video1_directo.gif"
               alt="Video Directo"
@@ -24,16 +24,16 @@ export default function SelectorModalidad({ onSelect }) {
             />
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          <h2 className="text-xl font-semibold mb-1">Simulación Online</h2>
+        <div className="flex flex-col items-center space-y-4">
+          <h2 className="text-xl font-semibold">Simulación Online</h2>
           <AnimatedButton
             onClick={() => onSelect("online")}
             className="bg-blue-600 text-white px-4 py-2 rounded shadow-lg hover:bg-blue-700"
           >
             Versión Online
           </AnimatedButton>
-          {/* Mostrar el GIF para la modalidad online */}
-          <div className="mt-2 w-64 h-36 bg-gray-200 flex items-center justify-center">
+          {/* GIF para modalidad online */}
+          <div className="w-64 h-36 bg-gray-200 flex items-center justify-center">
             <img
               src="/videos/video1_online.gif"
               alt="Video Online"
