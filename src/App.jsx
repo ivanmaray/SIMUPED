28c94e1875ef759f43d7dbf8e892807dee89e8e2
Iván Maray Mateos <ivanmaray@Ordenador-portatil-de-Ivan.local>
2025-04-01 23:11:04 +0200

    Actualizar logo

--- src/App.jsx
+++ src/App.jsx
@@ -158,7 +158,7 @@ export default function SimuPedApp() {
     <div className="min-h-screen flex flex-col justify-between bg-gradient-to-tr from-sky-100 via-white to-blue-200 p-6">
       <div className="bg-white shadow-xl rounded-2xl w-full max-w-3xl mx-auto p-4">
         {/* Encabezado: Solo el logo centrado */}
-        <div className="flex flex-col items-center mb-2">
+        <div className="flex flex-col items-center mb-1">
           <img
             src="/images/simuped_logo.png"
             alt="SimuPed Logo"
